5173667df9082015eddcc885b689a41d11b697ab
j8sh <j8shigarcia@aol.com>
2018-07-21 15:19:36 -0500

    completed challenge almost inc. sequence

---
+++ Almost Increasing Sequence/almostIncreasingSequence.js
@@ -0,0 +1,14 @@
+function almostIncreasingSequence(sequence) {
+    var count = 0;
+    for (var i = 0; i < sequence.length; i++) {
+        if (sequence[i] <= sequence[i - 1]) {
+            count++;
+            if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+                return false;
+            }
+        }
+    }
+    return count <= 1;
+}
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
